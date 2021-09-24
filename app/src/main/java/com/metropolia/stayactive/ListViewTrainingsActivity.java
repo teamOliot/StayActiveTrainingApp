5334486eef35d55cb4a688d43d35aea4816b7599
henkka <henrik.lappi@outlook.com>
2021-09-24 16:49:50 +0300

    Created Trainings class, TrainingDetailsActivity and the related xml. Created certain training delete button and its functionality.

--- app/src/main/java/com/metropolia/stayactive/ListViewTrainingsActivity.java
+++ app/src/main/java/com/metropolia/stayactive/ListViewTrainingsActivity.java
@@ -2,13 +2,55 @@ package com.metropolia.stayactive;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ListViewTrainingsActivity extends AppCompatActivity {
+    public static final String EXTRA = "com.metropolia.stayactive.EXTRA";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_view_trainings);
+
+        // Hardcoded training objects for testing
+        if(Trainings.getInstance().getTrainings().isEmpty()) {
+            Trainings.getInstance().getTrainings().add(new Training("juoksu", 15, 2021, 9, 24));
+            Trainings.getInstance().getTrainings().add(new Training("pyöräily", 45, 2021, 9, 24));
+        }
     }
+
+    @Override
+    protected void onResume () {
+        super.onResume();
+        ListView lv = findViewById(R.id.ListViewTrainings);
+
+        // Converts given list to individual list items
+        lv.setAdapter(new ArrayAdapter<Training>(
+                this,
+                R.layout.list_item_layout, //XML item layout
+                Trainings.getInstance().getTrainings()) //array of data
+        );
+
+        // when user clicks individual list item this method takes user to detailed view (TrainingDetailsActivity) about that list item
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("TAG", "onItemClick(" + i + ")");
+                Intent nextActivity = new Intent(ListViewTrainingsActivity.this, TrainingDetailsActivity.class);
+                // put i = index of list item to the next view
+                nextActivity.putExtra(EXTRA, i); // EXTRA is the key and i is the value
+                startActivity(nextActivity);
+            }
+        });
+    }
+
+
+
+
 }
