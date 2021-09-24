edc9d92eb82c1a5676c08b26ef8a14ed774f514a
KatjaDa <end.of.all89@gmail.com>
2021-09-24 14:08:14 +0300

    Added user profile activity and created views for it

---
+++ app/src/main/java/com/metropolia/stayactive/UserProfileActivity.java
@@ -0,0 +1,15 @@
+package com.metropolia.stayactive;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+
+    }
+}
