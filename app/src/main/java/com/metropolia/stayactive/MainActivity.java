34263293be2f1901e34b2d27b15e9b2f841b54ea
IinaLaa <iina.laamo@gmail.com>
2021-10-11 10:56:42 +0300

    Made final Javadoc comments.

--- app/src/main/java/com/metropolia/stayactive/MainActivity.java
+++ app/src/main/java/com/metropolia/stayactive/MainActivity.java
@@ -10,22 +10,39 @@ import android.widget.Button;
 
 import java.util.List;
 
+/**
+ * Main activity that contains methods for the main two activities: user profile and exercise diary
+ * @author Katja Dahlman
+ * @author Iina Laamo
+ * @author Henrik Lappi
+ * @version 10/2021
+ */
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * On create gets and displays the activity_main xml
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
 
-    // Takes user to UserProfileActivity
+    /**
+     * Method that takes the user to UserProfileActivity
+     * @param view
+     */
     public void toUserProfileView (View view) {
         Intent intent = new Intent(MainActivity.this, UserProfileActivity.class);
         Log.d("debug", "buttonUserProfile button works");
         startActivity(intent);
     }
 
-    // Takes user to ListViewTrainingsActivity
+    /**
+     * Method that takes user to ListViewTrainingsActivity
+     * @param view
+     */
     public void toTrainingsListView (View view) {
         Intent intent = new Intent(MainActivity.this, ListViewTrainingsActivity.class);
         Log.d("debug", "buttonTrainingsList button works");
--- app/src/main/java/com/metropolia/stayactive/SpinnerActivity.java
+++ app/src/main/java/com/metropolia/stayactive/SpinnerActivity.java
@@ -5,16 +5,31 @@ import android.view.View;
 import android.widget.AdapterView;
 
 // https://developer.android.com/guide/topics/ui/controls/spinner
+
+/**
+ * Spinner activity that utilizes a reaction from user
+ * @author Katja Dahlman
+ * @author Iina Laamo
+ * @author Henrik Lappi
+ * @version 10/2021
+ */
 public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
 
+    /**
+     * Method that is used to react to item selection
+     * @param parent
+     * @param view
+     * @param pos
+     * @param id
+     */
     public void onItemSelected(AdapterView<?> parent, View view,
                                int pos, long id) {
         // We override this method in AddNewTrainingActivity
-        /*Toast.makeText(parent.getContext(),
-                "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
-                Toast.LENGTH_SHORT).show();*/
     }
 
+    /**
+     * @param parent
+     */
     public void onNothingSelected(AdapterView<?> parent) {
         // Another interface callback
     }
